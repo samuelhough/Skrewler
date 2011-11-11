d63f8416c41d4421f27c24dc954d95c1e87349f4
Chris Abrams <chris@chrisabrams.org>
2011-11-10 19:28:15 -0600

    Updated README
    Signed-off-by: Chris Abrams <chris@chrisabrams.org>

--- jquery.skrewler.js
+++ jquery.skrewler.js
@@ -1,55 +1,23 @@
 /* 
- 
- Skrewler - A Jquery plugin for scrolling floated, unordered list elements vertically and horizontally
- Alpha 1.0
- Created by Sam Hough with code inspiration from Chris Abrams
 
- Open Source - MIT Licesnse 
- October 2011
+@name: Skrewler
+@plugin: jQuery
+@description: A jQuery plugin for scrolling list elements horizontally and vertically
+@author: Sam Hough
+@version: Alpha 1.1
+@updated: November 10th, 2011
+@contributors:
+Chris Abrams
 
- To use - call skrewler on your unorganized list, say whether it should scroll vertically or horizontall, and give the ids for your left/right or up/down
- buttons
- EX: $('#scroller').skrewler({ direction: "horizontal", leftUpButtonID: '#left', rightDownButtonID: '#right'});
+@license:
+Copyright (c) 2011 Samuel Hough
 
- Skrewler Options: 
-    REQUIRED: 
-        direction: "horizontal" || "vertical" - Direction skrewler will scroll your UL element
-    
-    REQUIRED (if enableKeys === false):
-        leftUpButtonID: "#id"                 - Selector ID for the left/Up button on your page
-        rightDownButtonID: "#id"              - Selector ID for the right/Down button on your page
-    
-    Optional:
-        duration: (int)              - How long the animations last
-        reverse: (boolean)           - Reverse the direction of the button / keyboard presses
-        enableKeys: (boolean)        - Enable keyboard control
-        keyCode_left: (int)          - Specify a different key code value to cause the list to move left
-        keyCode_right: (int)         - Specify a different key code value to cause the list to move right
-        keyCode_up: (int)            - Specify a different key code value to cause the list to move up
-        keyCode_down: (int)          - Specify a different key code value to cause the list to move down
-        postAnimFunc: function(){}   - Pass a function to be performed at the end of every scroll animation
-        leaveBoundsFunc: function(){}- Pass a function to be performed when the user tries to go outside a boundary (left or right/up down side)
-        scrollByPageSize: (boolean)  - Instead of scrolling by the average li elements width, scroll by the page width or height
- 
-// ----------------------------------------------------------------------------// 
-// Permission is hereby granted, free of charge, to any person obtaining a copy
-// of this software and associated documentation files ( the "Software" ), to deal
-// in the Software without restriction, including without limitation the rights
-// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-// copies of the Software, and to permit persons to whom the Software is
-// furnished to do so, subject to the following conditions:
-// 
-// The above copyright notice and this permission notice shall be included in
-// all copies or substantial portions of the Software.
-// 
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-// THE SOFTWARE. 
-// ----------------------------------------------------------------------------//      
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+     
 */
 
 (function($) {
